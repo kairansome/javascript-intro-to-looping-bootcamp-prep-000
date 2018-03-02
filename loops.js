be1dc09fd30ee016a0ffc1d3cedfb6ed6c9a4924
Kai Ransome <ransomekai@gmail.com>
2018-03-02 23:01:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,6 +21,6 @@ return ('done')
 function doWhileLoop(array) {
   function maybeTrue() {return Math.random() >= 0.5}
   
-  do { (array.slice (0,1) }
+  do { (array.slice(0,1)) }
   while (array.length > 0 && maybeTrue());
 }
